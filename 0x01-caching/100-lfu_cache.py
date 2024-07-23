6b79c7224215452a64a87f821f5849b812a5ff37
Asmaa26189 <engasmaahesham@gmail.com>
2024-07-23 23:59:20 +0300

    23-7-2024

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -64,7 +64,7 @@ class LFUCache(BaseCaching):
             self.__reorder_items(key)
 
     def get(self, key):
-        """Retrieves an item by key.
+        """get
         """
         if key is not None and key in self.cache_data:
             self.__reorder_items(key)
