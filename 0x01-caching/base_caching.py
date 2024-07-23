17b76e6489c95e0fc07a56e113a90ec4e821dac4
Asmaa26189 <engasmaahesham@gmail.com>
2024-07-23 21:39:09 +0300

    23-7-2024

--- 0x01-caching/base_caching.py
+++ 0x01-caching/base_caching.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env python3
-"""BaseCaching module.
-"""
+"""BaseCaching"""
 
 
 class BaseCaching():
-    """BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
+    """BaseCaching"""
     MAX_ITEMS = 4
 
     def __init__(self):
-        """Initiliazes the cache.
-        """
+        """Initiliazes"""
         self.cache_data = {}
 
     def print_cache(self):
-        """Prints the cache.
-        """
+        """Prints """
         print("Current cache:")
         for key in sorted(self.cache_data.keys()):
             print("{}: {}".format(key, self.cache_data.get(key)))
 
     def put(self, key, item):
-        """Adds an item in the cache.
-        """
+        """put"""
         error_msg = "put must be implemented in your cache class"
         raise NotImplementedError(error_msg)
 
     def get(self, key):
-        """Retrieves an item by key.
-        """
+        """get"""
         error_msg = "get must be implemented in your cache class"
         raise NotImplementedError(error_msg)
